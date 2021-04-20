537040b19698048fae202b058298746e662df8d0
Lubasia <lubasia.oasis@gmail.com>
2021-04-20 19:36:11 +0300

    no h2

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -4,7 +4,7 @@ import style from "../Statistics/Statistics.module.css";
 
 const Statistics = ({ title, stats }) => (
   <section className={style.statistics}>
-    <h2 className={style.title}>{title}</h2>
+    {title && <h2 className={style.title}>{title}</h2>}
 
     <ul className={style.stat__list}>
       {stats.map((stat) => (
